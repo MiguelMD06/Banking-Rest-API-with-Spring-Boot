739700a12670fb883c6d0141692945c23bb0b291
Miguel Medina Diaz <migue.a.medina06@gmail.com>
2026-06-14 19:34:15 -0500

    First commit: Add project files

---
+++ src/test/java/co/miguelmd06/bankingapp/BankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package co.miguelmd06.bankingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
