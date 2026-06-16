a484a4016509ce35ab2e2e1689612ad697894bd8
Miguel Medina Diaz <migue.a.medina06@gmail.com>
2026-06-16 11:50:37 -0500

    feat: Add a record class to involve details of potentials errors in API

--- src/main/java/co/miguelmd06/bankingapp/exception/ErrorDetails.java
+++ src/main/java/co/miguelmd06/bankingapp/exception/ErrorDetails.java
@@ -1,4 +1,11 @@
 package co.miguelmd06.bankingapp.exception;
 
-public record ErrorDetails() {
+import java.time.LocalDateTime;
+
+public record ErrorDetails(
+        LocalDateTime timestamp,
+        String errorMessage,
+        String errorDetails,
+        String httpCode
+) {
 }
